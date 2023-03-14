0562424c13f4fae5f4431b17d270fe1b27e02d1a
KubraKaradag23 <127855692+KubraKaradag23@users.noreply.github.com>
2023-03-14 16:45:51 +0300

    Create HW1.py

---
+++ HW1.py
@@ -0,0 +1,15 @@
+#String -> metinsel ifadeler
+#integer -> tam sayı ifadeleri
+#float double -> tam sayı olmayan numerik ifadeler
+#list,touple, range -> Sıralama ifadeleri
+#Boolean -> true false ifadeler
+#Bytes, bytearray, memoryview -> Binary veri ifadeleri
+
+#derslerin tamamlanmasından sonra geç butonuna basılması boolean ifadelerdir.
+#tamamlanan dersler sonrası tamamlandığını gösteren daire ifaleri de if bloklarıyla kontrol edilmektedir.
+
+yorumalani= "değerlendirmeler"
+if(yorumalani==None):
+    print ("Lütfen yoruma metin ekleyiniz")
+else:
+    print("Yorumum")
